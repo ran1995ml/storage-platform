9c2298aaa5c3f42d8c5c83a5111beba56e66a9e5
Ran Wei <49665849+ran1995ml@users.noreply.github.com>
2023-12-26 13:14:57 +0800

    Vue cli (#2)
    * 19
    
    * 25
    
    * login
    
    * valid

---
+++ storage-ui/src/api/index.js
@@ -0,0 +1,10 @@
+import http from '../utils/request'
+
+// 请求首页数据
+export const getData = () => {
+    return http.get('/home/getData')
+}
+
+export const getMenu = (data) => {
+    return http.post('/permission/getMenu', data)
+}
---
+++ storage-ui/src/components/CommonHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="header-container">
+    <div class="l-content">
+      <el-button style="margin-right: 20px" @click="handleMenu" icon="el-icon-menu" size="mini"></el-button>
+<!--      <el-breadcrumb separator="/">-->
+<!--        <el-breadcrumb-item v-for="item in tags" :key="item.path" :to="{ path: item.path }">{{ item.label }}</el-breadcrumb-item>-->
+<!--      </el-breadcrumb>-->
+    </div>
+    <div class="r-content">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          <img class="user" src="../assets/images/user-default.png" alt="">
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>Personal</el-dropdown-item>
+          <el-dropdown-item>Logout</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    handleMenu() {
+      this.$store.commit('collapseMenu')
+    }
+  },
+  computed: {
+    ...mapState({
+        tags: state => state.tab.tabsList
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.header-container {
+  padding: 0 20px;
+  background-color: #333;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .text {
+    color: white;
+    font-size: 14px;
+    margin-left: 10px;
+  }
+  .r-content {
+    .user {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+    }
+  }
+  .l-content {
+    display: flex;
+    align-items: center;
+    /deep/.el-breadcrumb__item {
+      .el-breadcrumb__inner {
+        font-weight: normal;
+        &.is-link {
+          color: #666;
+        }
+      }
+      &:last-child {
+        .el-breadcrumb__inner {
+          color: #fff;
+        }
+      }
+    }
+  }
+}
+</style>
